ebad060c42e61e5f2c8aa003c2b730aade830a8a
jieshenboy <917931079@qq.com>
2018-01-13 10:28:08 +0800

    add com

---
+++ com/joinPictures/joinHeroHeader.py
@@ -0,0 +1,27 @@
+##encoding = "utf-8"
+
+import math
+import PIL.Image as Image
+import os
+
+#打开拼接画板
+def joinPictures():
+    ls = os.listdir('D:\\heroavatar')
+    each_size = int(math.sqrt(float(640*640)/len(ls)))#每个图像的面积像素
+    lines = int(640/each_size)#每个图像的边长像素
+    image = Image.new('RGBA', (640, 640))
+    x = 0
+    y = 0
+    for i in range(0,len(ls)):
+        try:
+            img = Image.open(u'D:/heroavatar'+'/'+ls[i])
+        except IOError:
+            print("Error")
+        else:
+            img = img.resize((each_size, each_size), Image.ANTIALIAS)
+            image.paste(img, (x * each_size, y * each_size))
+            x += 1
+            if x == lines:
+                x = 0
+                y += 1
+    image.save(u'D:/heroavatar' + "/" + "all.png")
---
+++ com/main.py
@@ -0,0 +1,5 @@
+import com.getPictures.getHeroHeader as gP
+import com.joinPictures.joinHeroHeader as jP
+
+gP.getHeroHeader()
+jP.joinPictures()
